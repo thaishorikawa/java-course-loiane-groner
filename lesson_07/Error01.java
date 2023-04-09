45a54a9c0cc4ca6302e1852958f3fc10a4512bdd
thaishorikawa <thaishorikawa@hotmail.com>
2023-04-09 22:07:49 +0100

    chore: error 01 exercise

---
+++ lesson_07/Error01.java
@@ -0,0 +1,16 @@
+class Error01 {
+	
+	public static void main (String[] args){
+	
+		System.out.println("Hello World");
+	}
+}
+/*
+class Error01 {
+	
+	public static void main (String[] args){
+	
+		System.out.println("Hello World") //error: ';' expected
+	}
+}
+*/
