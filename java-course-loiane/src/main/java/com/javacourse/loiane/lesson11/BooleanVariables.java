9946f79bd74b154729fd94d1287c2d3cf98cc30e
thaishorikawa <thaishorikawa@hotmail.com>
2023-04-11 16:56:58 +0100

    chore: add variable exercises

---
+++ java-course-loiane/src/main/java/com/javacourse/loiane/lesson11/BooleanVariables.java
@@ -0,0 +1,14 @@
+package com.javacourse.loiane.lesson11;
+
+public class BooleanVariables {
+
+    public static void main(String[] args) {
+
+        boolean test1 = true;
+
+        boolean test2 = false;
+
+        System.out.println("Value of test1 is " + true);
+        System.out.println("Value of test2 is " + false);
+    }
+}
---
+++ java-course-loiane/src/main/java/com/javacourse/loiane/lesson11/CharVariables.java
@@ -0,0 +1,17 @@
+package com.javacourse.loiane.lesson11;
+
+public class CharVariables {
+
+    public static void main(String[] args) {
+
+        //char o = 'o';
+        //char i = 'i';
+
+        char o = 111;
+        char i = 105;
+        char questionMark = 0x3F;
+
+        System.out.println(""+o+i+questionMark);
+
+    }
+}
