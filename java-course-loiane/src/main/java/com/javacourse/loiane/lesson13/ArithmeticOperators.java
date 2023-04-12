191add438b74882e2bbdfdd8ee27aa75e8769b63
thaishorikawa <thaishorikawa@hotmail.com>
2023-04-12 09:59:01 +0100

    chore: add arithmetic operators exercise

---
+++ java-course-loiane/src/main/java/com/javacourse/loiane/lesson13/ArithmeticOperators.java
@@ -0,0 +1,35 @@
+package com.javacourse.loiane.lesson13;
+
+public class ArithmeticOperators {
+
+    public static void main(String[] args) {
+
+        int result = 1 + 2;
+        System.out.println(result);
+
+        result = result - 1;
+        System.out.println(result);
+
+        result = result * 2;
+        System.out.println(result);
+
+        result = result / 2;
+        System.out.println(result);
+
+        result = result + 8;
+        System.out.println(result);
+
+        result = result % 7;
+        System.out.println(result);
+
+        result += 1;
+        System.out.println(result++);
+
+        result -= 3;
+        System.out.println(result);
+
+        System.out.println(result--);
+        System.out.println(--result);
+        
+    }
+}
