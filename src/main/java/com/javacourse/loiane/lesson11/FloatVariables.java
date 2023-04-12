e8487536f4e8c52c5148720c8c41783b4df2fed0
thaishorikawa <thaishorikawa@hotmail.com>
2023-04-12 22:12:40 +0100

    fix: move all code to initial folder

---
+++ src/main/java/com/javacourse/loiane/lesson11/FloatVariables.java
@@ -0,0 +1,15 @@
+package com.javacourse.loiane.lesson11;
+
+public class FloatVariables {
+
+    public static void main(String[] args) {
+
+        double ticketPrice = 2.90;
+
+        float tomatoPrice = 3.95F;
+
+        System.out.println("Ticket price = " + ticketPrice);
+
+        System.out.println("Tomato price = " + tomatoPrice);
+    }
+}
