e1426ae4cda2703242cd6ee934fb20c68b5beb0d
thaishorikawa <thaishorikawa@hotmail.com>
2023-04-15 17:59:10 +0100

    chore: add for loop  exercises

---
+++ src/main/java/com/javacourse/loiane/lesson17/ForLoopTwoVariables.java
@@ -0,0 +1,11 @@
+package com.javacourse.loiane.lesson17;
+
+public class ForLoopTwoVariables {
+
+    public static void main(String[] args) {
+
+        for (int i = 0, j = 10; i < j; i++, j--)
+            System.out.println("i = " + i + "; j = " + j);
+
+    }
+}
