2c3c7e096955d9b08ff5267e381f9918bc404053
thaishorikawa <thaishorikawa@hotmail.com>
2023-04-17 20:43:29 +0100

    chore: add matrix exercise

---
+++ src/main/java/com/javacourse/loiane/lesson20/Matrix.java
@@ -0,0 +1,24 @@
+package com.javacourse.loiane.lesson20;
+
+public class Matrix {
+
+    public static void main(String[] args) {
+
+        double[][] studentsGrades = new double[30][4];
+
+        studentsGrades[0][0] = 10;
+        studentsGrades[0][1] = 7;
+        studentsGrades[0][2] = 9;
+        studentsGrades[0][3] = 9.5;
+
+        studentsGrades[1][0] = 9;
+        studentsGrades[1][1] = 8;
+        studentsGrades[1][2] = 7;
+        studentsGrades[1][3] = 9;
+
+        studentsGrades[2][0] = 8;
+        studentsGrades[2][1] = 9;
+        studentsGrades[2][2] = 10;
+        studentsGrades[2][3] = 7.5;
+    }
+}
