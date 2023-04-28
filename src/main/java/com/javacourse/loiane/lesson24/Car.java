720581636d94ff786867a3cb38a62d798ec60825
thaishorikawa <thaishorikawa@hotmail.com>
2023-04-28 16:49:39 +0100

    chore: add oop example exercise

---
+++ src/main/java/com/javacourse/loiane/lesson24/Car.java
@@ -0,0 +1,11 @@
+package com.javacourse.loiane.lesson24;
+
+public class Car {
+
+    String brand;
+    String model;
+    int numPassengers = 10;
+    double fuelCapacity = 100;
+    double fuelConsumption = 0.2;
+
+}
---
+++ src/main/java/com/javacourse/loiane/lesson24/CarTest.java
@@ -0,0 +1,16 @@
+package com.javacourse.loiane.lesson24;
+
+public class CarTest {
+
+    public static void main(String[] args) {
+
+        Car van = new Car();
+        van.brand = "Fiat";
+        van.model = "Ducato";
+        van.numPassengers = 10;
+        van.fuelCapacity = 100;
+        van.fuelConsumption = 0.2;
+
+        System.out.println(van.brand);
+    }
+}
