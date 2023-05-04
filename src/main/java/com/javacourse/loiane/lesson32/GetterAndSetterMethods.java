6349b6be1628fce3819068d63890be787e1bacf8
thaishorikawa <thaishorikawa@hotmail.com>
2023-05-04 17:50:55 +0100

    chore: add getter and setter methods exercise

---
+++ src/main/java/com/javacourse/loiane/lesson32/GetterAndSetterMethods.java
@@ -0,0 +1,50 @@
+package com.javacourse.loiane.lesson32;
+
+public class GetterAndSetterMethods {
+
+    private String brand;
+    private String model;
+    private int numPassengers;
+    private double fuelCapacity;
+    private double fuelConsumption;
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public int getNumPassengers() {
+        return numPassengers;
+    }
+
+    public void setNumPassengers(int numPassengers) {
+        this.numPassengers = numPassengers;
+    }
+
+    public double getFuelCapacity() {
+        return fuelCapacity;
+    }
+
+    public void setFuelCapacity(double fuelCapacity) {
+        this.fuelCapacity = fuelCapacity;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+}
---
+++ src/main/java/com/javacourse/loiane/lesson32/GetterAndSetterMethodsTest.java
@@ -0,0 +1,14 @@
+package com.javacourse.loiane.lesson32;
+
+public class GetterAndSetterMethodsTest {
+
+    public static void main(String[] args) {
+
+        GetterAndSetterMethods test = new GetterAndSetterMethods();
+
+        test.setBrand("Fiat");
+
+        System.out.println(test.getBrand());
+
+    }
+}
